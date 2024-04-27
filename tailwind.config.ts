3067aea669c6442aba49adb56df9da036d2740df
Joo Chanhwi <56245920+te6-in@users.noreply.github.com>
2024-04-27 23:53:20 +0900

    chore: @te6/ui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,31 @@
+import { te6TailwindPreset } from "@te6/ui";
 import { type Config } from "tailwindcss";
+import { amber, blue, neutral } from "tailwindcss/colors";
 import { fontFamily } from "tailwindcss/defaultTheme";
 
 export default {
-  content: ["./src/**/*.tsx"],
+  content: ["./src/**/*.tsx", "./node_modules/@te6/ui/dist/index.js"],
+  presets: [te6TailwindPreset],
   theme: {
+    colors: {
+      body: {
+        DEFAULT: "#f7f7f7",
+        dark: "#1f1f1f",
+      },
+      base: neutral,
+      "base-dark": neutral,
+      primary: blue,
+      accent: amber,
+    },
     extend: {
+      minHeight: {
+        // @ts-expect-error this can be an array
+        screen: ["100vh", "100dvh"],
+      },
+      height: {
+        // @ts-expect-error this can be an array
+        screen: ["100vh", "100dvh"],
+      },
       fontFamily: {
         sans: ["Pretendard\\ Variable", "Pretendard", ...fontFamily.sans],
       },
