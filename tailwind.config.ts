0054cabadbdc467c50b13513fcb18b84cc62af75
Joo Chanhwi <56245920+te6-in@users.noreply.github.com>
2024-04-29 00:13:13 +0900

    fix: fix tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,17 +7,17 @@ export default {
   content: ["./src/**/*.tsx", "./node_modules/@te6/ui/dist/index.js"],
   presets: [te6TailwindPreset],
   theme: {
-    colors: {
-      body: {
-        DEFAULT: "#f7f7f7",
-        dark: "#1f1f1f",
-      },
-      base: neutral,
-      "base-dark": neutral,
-      primary: blue,
-      accent: amber,
-    },
     extend: {
+      colors: {
+        body: {
+          DEFAULT: "#f7f7f7",
+          dark: "#1f1f1f",
+        },
+        base: neutral,
+        "base-dark": neutral,
+        primary: blue,
+        accent: amber,
+      },
       minHeight: {
         // @ts-expect-error this can be an array
         screen: ["100vh", "100dvh"],
