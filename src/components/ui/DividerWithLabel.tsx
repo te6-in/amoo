7f0021fd6c1091c6afd89c2cc6a9e9d8d3e04762
Joo Chanhwi <56245920+te6-in@users.noreply.github.com>
2024-05-02 00:12:32 +0900

    feat: `DividerWithLabel`

---
+++ src/components/ui/DividerWithLabel.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { j } from "@te6/ui";
+
+// FIXME: why this should be a client-side component?
+
+interface DividerWithLabelProps {
+  text?: string;
+  className?: string;
+}
+
+export function DividerWithLabel({ text, className }: DividerWithLabelProps) {
+  return (
+    <div className="relative">
+      <hr className="border-neutral-200 dark:border-neutral-800" />
+      <span
+        className={j(
+          "absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 font-semibold text-xs text-neutral-400 px-2",
+          className ?? "bg-body dark:bg-body-dark",
+        )}
+      >
+        {text ?? "또는"}
+      </span>
+    </div>
+  );
+}
