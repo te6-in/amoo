b07b6141dd8391636b63139da87051d00f31b3c6
Joo Chanhwi <56245920+te6-in@users.noreply.github.com>
2024-05-06 00:04:02 +0900

    feat: redirect users from `/` to `/auth` / `/dashboard`

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,18 @@
-export default function HomePage() {
-  return <div>Home</div>;
+import { createServerClient } from "@/libs/supabase/server";
+
+import { redirect } from "next/navigation";
+
+export default async function HomePage() {
+  const supabase = createServerClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) {
+    redirect("/auth");
+  }
+
+  redirect("/dashboard");
+
+  return;
 }
